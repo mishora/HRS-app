344f44c9edc69332a9e01372c8ce53d995a0f7a1
Mihail Kovachev <mishoria@gmail.com>
2025-02-06 22:15:02 +0200

    delete github actions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.set('view engine', 'pug');
 app.use(require('body-parser')
   .urlencoded({extended:true}));
 
-const studentsController = 
+const studentsController =
   require("./controllers/students-controller");
 
 let students = require("./models/students-model");
@@ -13,7 +13,7 @@ studentsController.setup(app, students);
 
 let port = process.argv[2];
 if (!port) port = process.env['PORT'];
-if (!port) port = 8080;
+if (!port) port = 8081;
 
 app.listen(port, () => {
   console.log(`App started. Listening at http://localhost:${port}`);
@@ -21,6 +21,6 @@ app.listen(port, () => {
 .on('error', function(err) {
   if (err.errno === 'EADDRINUSE')
     console.error(`Port ${port} busy.`);
-  else 
+  else
     throw err;
 });
